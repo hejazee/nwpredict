8d164de2bb38b0bbafeb4eb5e4b654bf21a44d24
Ahmad Hejazee <mngafa@gmail.com>
2015-11-14 23:08:22 +0330

    Added UI : simple

---
+++ ui/scripts/nwpredict.js
@@ -0,0 +1,11 @@
+(function($) {
+  $(function() {
+    $('#inputbox').keypress(function(e) {
+      if (e.key == ' ' || e.key == 'Tab' || e.key == 'Enter') {
+        // blank inserted
+        // Process suggestions
+        
+      }
+    });
+  });
+})(jQuery);
