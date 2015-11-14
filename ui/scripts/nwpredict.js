58055a3677abf06ca5b885f15fb3ab918b4cc363
Ahmad Hejazee <mngafa@gmail.com>
2015-11-14 23:57:24 +0330

    completed web ui basic functionality

--- ui/scripts/nwpredict.js
+++ ui/scripts/nwpredict.js
@@ -17,7 +17,17 @@
         // Get suggestions from server
         var url = ['../predictor.php?phrase=', phrase].join('');
         $.getJSON(url, function(suggestions) {
-          console.log(suggestions);
+          //console.log(suggestions);
+          for (var i = 0; i < 3; i++) {
+            var sug_id = ['#sug', i + 1].join('');
+            var sug = $(sug_id);
+            if (i < suggestions.length) {
+              sug.text(suggestions[i]);
+            }
+            else {
+              sug.html('&nbsp;');
+            }
+          }
         })
         .fail(function() {
           // Javascript promise: Error occured
